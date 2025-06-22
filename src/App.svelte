3fe13c99dad96d0462ceb532d2f9e9f193c528f4
zummon <zummon.space@gmail.com>
2025-06-22 23:43:51 +0700

    up

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,33 @@
 <script>
-	import { onMount } from 'svelte'
+	import { onMount } from "svelte";
 
-	let repeat = $state(76)
-	let ts = $state(["title", "subtitle"])
-	let ws = $state([500, 500])
-	let lss = $state([0, 0])
+	let repeat = $state(76);
+	let ts = $state(["title", "subtitle"]);
+	let ws = $state([500, 500]);
+	let lss = $state([0, 0]);
 
-	let cards = $state([
+	let chunks = $state([
 		{
-			key: '',
-			write: '',
-			heavy: 500,
-			space: 0,
+			lines: ["title", "subtitle"],
+			heavies: [500, 500],
+			spaces: [0, 0],
+			repeat: 76,
 		},
-	])
+		{
+			lines: ["exclusive", "Mr.Tee", "Human"],
+			heavies: [500, 500, 500],
+			spaces: [0, 0, 0],
+			repeat: 4,
+		},
+	]);
 
 	onMount(() => {
-		const searchParams = new URLSearchParams(location.search)
+		const searchParams = new URLSearchParams(location.search);
 		let prepare = {
 			ts: searchParams.get("t"),
 			ws: searchParams.get("w"),
-			lss: searchParams.get("ls")
-		}
+			lss: searchParams.get("ls"),
+		};
 		if (prepare.ts) {
 			ts = prepare.ts.split(",");
 		}
@@ -31,40 +37,70 @@
 		if (prepare.lss) {
 			lss = prepare.lss.split(",");
 		}
-	})
-
+	});
 </script>
 
 <div class="flex flex-wrap justify-center gap-2 p-4 print:hidden">
 	<div class="">
-		<label class="inline-block border-2 border-teal-500 font-medium text-teal-600">
+		<label
+			class="inline-block border-2 border-teal-500 font-medium text-teal-600"
+		>
 			<span class="pl-2">Repeat</span>
-			<input class="bg-transparent text-center p-1 w-20" type="number" min="1" max="99" bind:value={repeat}>
+			<input
+				class="bg-transparent text-center p-1 w-20"
+				type="number"
+				min="1"
+				max="99"
+				bind:value={repeat}
+			/>
 		</label>
 	</div>
 	{#each ts as part, index}
 		<div class="flex flex-wrap gap-2">
 			<div class="">
-				<input class="bg-transparent text-center p-1 border-2 border-teal-500" type="text" bind:value={ts[index]}>
+				<input
+					class="bg-transparent text-center p-1 border-2 border-teal-500"
+					type="text"
+					bind:value={ts[index]}
+				/>
 			</div>
 			<div class="">
-				<input class="bg-transparent text-center p-1 w-20 border-2 border-teal-500" type="number" step="100" max="1000" min="100" bind:value={ws[index]}>
+				<input
+					class="bg-transparent text-center p-1 w-20 border-2 border-teal-500"
+					type="number"
+					step="100"
+					max="1000"
+					min="100"
+					bind:value={ws[index]}
+				/>
 			</div>
 			<div class="">
-				<input class="bg-transparent text-center p-1 w-20 border-2 border-teal-500" type="number" step="0.01" bind:value={lss[index]}>
+				<input
+					class="bg-transparent text-center p-1 w-20 border-2 border-teal-500"
+					type="number"
+					step="0.01"
+					bind:value={lss[index]}
+				/>
 			</div>
 		</div>
 	{/each}
 </div>
 
-<div class="flex flex-wrap justify-center gap-y-2 gap-x-3 text-center px-4 print:px-0 mx-auto max-w-[877px] print:max-w-none" style="font-family: 'Kodchasan', sans-serif">
+<div
+	class="flex flex-wrap justify-center gap-y-2 gap-x-3 text-center px-4 print:px-0 mx-auto max-w-[877px] print:max-w-none"
+	style="font-family: 'Kodchasan', sans-serif"
+>
 	{#each Array.from({ length: repeat }) as i}
 		<div class="">
 			{#each ts as part, index}
-				<div class="" style="font-weight: {isNaN(ws[index]) ? 400 : ws[index] }; letter-spacing: {isNaN(lss[index]) ? 0 : lss[index] }em;" >{ts[index]}</div>
+				<div
+					class=""
+					style:font-weight={isNaN(ws[index]) ? 400 : ws[index]}
+					style:letter-spacing={(isNaN(lss[index]) ? 0 : lss[index]) + "em"}
+				>
+					{ts[index]}
+				</div>
 			{/each}
 		</div>
 	{/each}
 </div>
-
-
